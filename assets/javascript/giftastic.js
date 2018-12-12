4f3aa5ebd52dcd0050fcabc825d70efc0435f29d
Riley Brown <brown.riley.c@gmail.com>
2018-12-12 18:15:38 -0500

    Added stop and start onclick event.  Displayed metadata.  Need to add mediaQueries, need to add extra styling, need to do localstorage stuff for favorite button.

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,27 +1,41 @@
 $(document).ready(function(){
     var person;
     
-    var tagArray = ["Jean Luc Picard", "Neo", "Tyrion Lannister", "Malcolm Reynolds"];
-    
-
+    var tagArray = ["Patrick Stewart", "Keanu Reeves", "Peter Dinklage", "Nathan Fillion"];    
+    var favoriteArray = [];
     // Possibly can create metaDataArray so as to dry up the createGifDivs
-    
+  
     var createGifDivs = function(num) {
         // Runs inside a for loop in the then portion of AJAX
         // Creates a ".gif"+i div and appends it to the ".allGifDiv"
         // Next it creates three more divs, metaDiv, imageDiv, and manipulateDiv and appends them to the ".gif"+i
         var gifDiv = $('<div>');
         gifDiv.appendTo('.allGifDiv').addClass('gifDiv gif'+num);
-        var metaDiv = $('<div>').addClass('meta'+num);
+        
+        var metaDiv = $('<div>').addClass('meta');
+        var metaRatingDiv = $('<div>').addClass('metaDivs rating'+num);
+        var metaTitleDiv = $('<div>').addClass('metaDivs title'+num);
+        var metaSourceDiv = $('<div>').addClass('metaDivs source'+num);
+        metaDiv.append(metaRatingDiv, metaTitleDiv, metaSourceDiv);
+
+        var imageDiv = $('<img>').addClass('gif image'+num);
+
+        var manipulateDiv = $('<div>').addClass('manipulate'+num);
+        var downloadButton = $('<button>').addClass('manipulateDiv download'+num).text("Download");
+        var favoriteButton = $('<button>').addClass('manipulateDiv favoriteButton'+num).text("🖤");
+        manipulateDiv.append(downloadButton, favoriteButton);
         // Here I need to add more divisions to separate out and place the appropriate metaData I want to use
         // Maybe separate these out into their own functions so I can keep createGifDivs small and clean looking?
         // I also need to add the buttons and onclicks for the manipulate div
-        var imageDiv = $('<img>').addClass('image'+num);
-        var manipulateDiv = $('<div>').addClass('manipulate'+num);
+        
+        
         $('.gif'+num).append(metaDiv, imageDiv, manipulateDiv);
     };
 
-   
+   var storeFavoriteClick = function(num) {
+        // Save the values of the various divs, and save them to an array, stringify the array and store in localstorage
+        // 
+   }
 
     var createTagButtons = function(){
         for (var i = 0; i< tagArray.length; i ++) {
@@ -38,10 +52,12 @@ $(document).ready(function(){
                     method: "GET"
                 }).then(function(response){
                     console.log(response);
-
                     for (var j = 0; j < response.data.length; j ++) {
                         createGifDivs(j);
-                        $('.image'+j).attr("src",response.data[j].images.fixed_height.url);
+                        $('.image'+j).attr("src",response.data[j].images.fixed_height_still.url).attr("data-animate", response.data[j].images.fixed_height.url).attr("data-still", response.data[j].images.fixed_height_still.url).attr("data-state", "still");
+                        $('.rating'+j).text(response.data[j].rating);
+                        $('.title'+j).text(response.data[j].title);
+                        $('.source'+j).html('<a href:"'+response.data[j].source+'"</a>')
                     };
                 })
             })
@@ -60,5 +76,19 @@ $(document).ready(function(){
 
         createTagButtons();
     })
+    
+    $('.allGifDiv').on('click','.gif', function(){
+        var currentState = $(this).attr('data-state');
+        if (currentState === "still") {
+            var animated = $(this).attr("data-animate");
+            $(this).attr('src',animated);
+            $(this).attr("data-state", "animated");
+        };
+        if (currentState === "animated") {
+            var still = $(this).attr("data-still");
+            $(this).attr("src",still);
+            $(this).attr("data-state","still");
+        };
+    });
     createTagButtons();
 });
