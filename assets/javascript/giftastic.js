4d6ff4df0bff65968cc6289a9f20dbab274c7033
Riley Brown <brown.riley.c@gmail.com>
2018-12-12 19:37:37 -0500

    Reduced metadata elements to just rating.  Got localstorage to work on favorites button.  Still need styling and download button.  What API functionality should I add?

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -2,40 +2,31 @@ $(document).ready(function(){
     var person;
     
     var tagArray = ["Patrick Stewart", "Keanu Reeves", "Peter Dinklage", "Nathan Fillion"];    
-    var favoriteArray = [];
+    var favoriteStillImageArray = [];
+    var favoriteAnimatedImageArray = [];
+    var favoriteRatingArray = [];
+
     // Possibly can create metaDataArray so as to dry up the createGifDivs
   
     var createGifDivs = function(num) {
-        // Runs inside a for loop in the then portion of AJAX
-        // Creates a ".gif"+i div and appends it to the ".allGifDiv"
-        // Next it creates three more divs, metaDiv, imageDiv, and manipulateDiv and appends them to the ".gif"+i
         var gifDiv = $('<div>');
         gifDiv.appendTo('.allGifDiv').addClass('gifDiv gif'+num);
         
         var metaDiv = $('<div>').addClass('meta');
         var metaRatingDiv = $('<div>').addClass('metaDivs rating'+num);
-        var metaTitleDiv = $('<div>').addClass('metaDivs title'+num);
-        var metaSourceDiv = $('<div>').addClass('metaDivs source'+num);
-        metaDiv.append(metaRatingDiv, metaTitleDiv, metaSourceDiv);
+        // var metaTitleDiv = $('<div>').addClass('metaDivs title'+num);
+        // var metaSourceDiv = $('<div>').addClass('metaDivs source'+num);
+        metaDiv.append(metaRatingDiv);
 
         var imageDiv = $('<img>').addClass('gif image'+num);
 
         var manipulateDiv = $('<div>').addClass('manipulate'+num);
         var downloadButton = $('<button>').addClass('manipulateDiv download'+num).text("Download");
-        var favoriteButton = $('<button>').addClass('manipulateDiv favoriteButton'+num).text("🖤");
+        var favoriteButton = $('<button>').addClass('manipulateDiv favoriteButton').text("🖤").attr('data-number',num);
         manipulateDiv.append(downloadButton, favoriteButton);
-        // Here I need to add more divisions to separate out and place the appropriate metaData I want to use
-        // Maybe separate these out into their own functions so I can keep createGifDivs small and clean looking?
-        // I also need to add the buttons and onclicks for the manipulate div
-        
         
         $('.gif'+num).append(metaDiv, imageDiv, manipulateDiv);
     };
-
-   var storeFavoriteClick = function(num) {
-        // Save the values of the various divs, and save them to an array, stringify the array and store in localstorage
-        // 
-   }
 
     var createTagButtons = function(){
         for (var i = 0; i< tagArray.length; i ++) {
@@ -54,10 +45,10 @@ $(document).ready(function(){
                     console.log(response);
                     for (var j = 0; j < response.data.length; j ++) {
                         createGifDivs(j);
-                        $('.image'+j).attr("src",response.data[j].images.fixed_height_still.url).attr("data-animate", response.data[j].images.fixed_height.url).attr("data-still", response.data[j].images.fixed_height_still.url).attr("data-state", "still");
+                        $('.image'+j).attr("src",response.data[j].images.fixed_height_still.url).attr("data-animated", response.data[j].images.fixed_height.url).attr("data-still", response.data[j].images.fixed_height_still.url).attr("data-state", "still");
                         $('.rating'+j).text(response.data[j].rating);
-                        $('.title'+j).text(response.data[j].title);
-                        $('.source'+j).html('<a href:"'+response.data[j].source+'"</a>')
+                        // $('.title'+j).text(response.data[j].title);
+                        // $('.source'+j).html('<a href:"'+response.data[j].source+'"> Source </a>')
                     };
                 })
             })
@@ -76,11 +67,11 @@ $(document).ready(function(){
 
         createTagButtons();
     })
-    
+
     $('.allGifDiv').on('click','.gif', function(){
         var currentState = $(this).attr('data-state');
         if (currentState === "still") {
-            var animated = $(this).attr("data-animate");
+            var animated = $(this).attr("data-animated");
             $(this).attr('src',animated);
             $(this).attr("data-state", "animated");
         };
@@ -90,5 +81,32 @@ $(document).ready(function(){
             $(this).attr("data-state","still");
         };
     });
+
+    $('.allGifDiv').on('click','.favoriteButton', function(){
+        var gifNumber = $(this).attr('data-number');
+        var favoriteRating = $('.rating'+gifNumber).text();
+        var favoriteStillImage = $('.image'+gifNumber).attr('data-still');
+        var favoriteAnimatedImage = $('.image'+gifNumber).attr('data-animated');
+        favoriteRatingArray.push(favoriteRating);
+        favoriteStillImageArray.push(favoriteStillImage);
+        favoriteAnimatedImageArray.push(favoriteAnimatedImage);
+
+        localStorage.clear();
+        localStorage.setItem("ratingList",JSON.stringify(favoriteRatingArray));
+        localStorage.setItem("stillImageList",JSON.stringify(favoriteStillImageArray));
+        localStorage.setItem("animatedImageList",JSON.stringify(favoriteAnimatedImageArray));
+    });
+
+    $('.showFavoritesButton').on('click', function(){
+        favoriteRatingArray = JSON.parse(localStorage.getItem("ratingList"));
+        favoriteStillImageArray = JSON.parse(localStorage.getItem('stillImageList'));
+        favoriteAnimatedImageArray = JSON.parse(localStorage.getItem("animatedImageList"));
+        $('.allGifDiv').empty();
+        for (var k = 0; k < favoriteRatingArray.length; k++) {
+            createGifDivs(k);
+            $('.image'+k).attr("src",favoriteStillImageArray[k]).attr("data-animated", favoriteAnimatedImageArray[k]).attr("data-still", favoriteStillImageArray[k]).attr("data-state", "still");
+            $('.rating'+k).text(favoriteRatingArray[k]);
+        }
+    })
     createTagButtons();
 });
